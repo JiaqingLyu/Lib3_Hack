4d110831dfe0015c980d0359dbee8e0841e362ba
roast_racoon <798175437@qq.com>
2019-11-08 22:19:22 -0600

    tab1 / tab2 changes
    finish the approximate page of tab1 and tab2

---
+++ CampusConnect/src/app/dummy data/classes.ts
@@ -0,0 +1,17 @@
+export const classes = [
+	{
+		name: 'Digital image processing',
+		teacher: 'Zhu Li',
+		description: ''
+	},
+	{
+		name: 'Algorithm and Complexity',
+		teacher: 'Huy',
+		description: ''
+	},
+	{
+		name: 'intro to statistic learning',
+		teacher: 'mitchell ken',
+		description: ''
+	}
+];
--- CampusConnect/src/app/tab1/tab1.page.ts
+++ CampusConnect/src/app/tab1/tab1.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import {classes} from '../dummy data/classes'
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  classes=classes;
   constructor() {}
 
 }
