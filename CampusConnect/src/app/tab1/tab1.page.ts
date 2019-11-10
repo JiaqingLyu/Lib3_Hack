d0c8e5f2848f5350ee085af78f7802ff85faeb8f
Hanson <a190884810@gmail.com>
2019-11-09 20:46:16 -0600

    change to tab1

--- CampusConnect/src/app/tab1/tab1.page.ts
+++ CampusConnect/src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import {classes} from '../dummy data/classes'
+import {HttpClient} from '@angular/common/http';
+import {CoursesService} from '../courses.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  classes=classes;
-  constructor() {}
-
+  data: any;
+  constructor(private coursesService : CoursesService) {}
+  ngOnInit() {
+    this.coursesService
+      .getCourses()
+      .subscribe(data => {
+        this.data = data.split(',');
+        console.log(this.data[2]);
+        console.log(this.data[3]);
+    });
+  }
 }
